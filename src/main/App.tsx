42ba38cdfb52151b0f7543dc32d55519df1f3852
ronyman.com <ronyman.com@outlook.com>
2023-08-11 22:15:01 +1000

    start build

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -1,30 +1,24 @@
-import { useState } from 'react'
 import 'bulma/css/bulma.css'
 import RonyManLogo from '../assets/ronyman_logo.svg'
 import RonyPicture from '../assets/Rony.jpeg'
 import './App.css'
-import Vue from 'vue'
-import VuePrism from 'vue-prism'
-Vue.use(VuePrism)
-
-import 'prismjs/themes/prism.css'
 
 function App() {
   return (
     <>
-      <div class="">
+      <div className="container">
         <a href="/" target="_blank">
         </a>
         <a href="/" target="_blank">
           <img src={RonyManLogo} className="logo ronyman" alt="RonyMAN LOGO" />
         </a>
 
-      <h1 class="ronyman-com">ronyman.com</h1>
-      <h3 class="ronyman-com">services@ronyman.com</h3>
-      <div class="diagonal-box bg-one">
+      <h1 className="ronyman-com">ronyman.com</h1>
+      <h3 className="ronyman-com">services@ronyman.com</h3>
+      <div className="diagonal-box bg-one">
        <img src={RonyPicture} className="RonyPicture" alt="RonyMAN" />
-  <div class="container">
-    <p class="ronyman-com">
+  <div className="container">
+    <p className="ronyman-com">
       <hr />
       Checkout <a href="https://amatak.org/">Amatak</a> All my projects out there. It seem i have no idea to descript here.
     </p>
@@ -32,8 +26,8 @@ function App() {
 
   </div>
 </div>
-<div class="diagonal-box bg-two">
-  <div class="ronyman-com">
+<div className="diagonal-box bg-two">
+  <div className="ronyman-com">
     <h2>Server Repo</h2>
     <p>
       <a href="https://github.com/amatak-org" target="_blank">GitHub</a>
--- src/main/main.tsx
+++ src/main/main.tsx
@@ -2,11 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import Vue from 'vue'
-import VuePrism from 'vue-prism'
-Vue.use(VuePrism)
-
-import 'prismjs/themes/prism.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
